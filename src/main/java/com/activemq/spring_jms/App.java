6008e0f0a87c4c10d3b86aba500f859aece922bb
quanchunlindellpc <quanchunlindellpc@163.com>
2018-01-02 14:15:48 +0800

    使用线程池生产消息

--- src/main/java/com/activemq/spring_jms/App.java
+++ src/main/java/com/activemq/spring_jms/App.java
@@ -3,26 +3,25 @@ package com.activemq.spring_jms;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
- * Hello world!
- *
+ * Spring Jms 演示 
  */
-public class App 
-{
+public class App {
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
 	
     private static ClassPathXmlApplicationContext context;
     
 	public static MessageSendService messageSendService;
     
-    public static void main( String[] args )
-    {
-    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"spring-context.xml"}); 
+    public static void main( String[] args ) {
+    	
+    	context = new ClassPathXmlApplicationContext(new String[] {"spring-context.xml"}); 
     	
     	messageSendService = (MessageSendService) context.getBean("messageSendService");
-    	
-    	System.out.println("测试消息 ---> Hello world");
-    	messageSendService.sendMessage("测试消息 ---> Hello world");
+
+    	//基本方式发送消息
+    	messageSendService.sendMessage("测试消息 -> Normal");
+    	//使用线程池发送消息
+    	messageSendService.sendMessageUsingThreadPool("测试消息 -> ThreadPool");
     }
 }
--- src/main/java/com/activemq/spring_jms/CustomThreadMessageListener.java
+++ src/main/java/com/activemq/spring_jms/CustomThreadMessageListener.java
@@ -4,16 +4,27 @@ import javax.jms.*;
 import org.apache.activemq.command.ActiveMQTextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 public class CustomThreadMessageListener implements MessageListener {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomThreadMessageListener.class);
 
-	public void onMessage(Message msg) {
+	public void onMessage(Message message) {
 		
 		try {
-			
-			LOGGER.info("消费 测试信息： {}", ((ActiveMQTextMessage) msg).getText());
+			ActiveMQTextMessage msg = (ActiveMQTextMessage) message;
+	        final String jsonString = msg.getText();
+
+	        JSON jsonData = (JSON)JSONObject.parse(jsonString);
+	        TestVO testVO = JSONObject.toJavaObject(jsonData, TestVO.class);
+            if (testVO == null) {
+                return;
+            }
+            
+	        System.out.println("消费" + testVO.getName());
+	        
         } catch (JMSException e) {
             e.printStackTrace();
         }
